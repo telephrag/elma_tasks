bd673802bcbf2f93a638803da3517fae4ce14dab
trofchik <trofimov.nikita20@gmail.com>
2021-10-27 15:11:51 +0400

    Cleanup

--- cyclicrotation/cyclicrotation.go
+++ cyclicrotation/cyclicrotation.go
@@ -1,22 +1,5 @@
 package cyclicrotation
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
-
-// 1. Save (n - k) to (n - 1) elements
-// 2. Move (0) to (n - k - 1) elements to the right. Iterate right to left.
-// 3. Move k elements from 1" to the left. Iterate left to right.
-// 4. 1" and 2" probably should be reversed when k < n/2
-
-func measureTime(f func([]int, int) []int, A []int, k int) time.Duration {
-	var start = time.Now()
-	f(A, k)
-	return time.Since(start)
-}
-
 func Solution(A []int, k int) []int {
 	var temp int
 	var N int = len(A)
@@ -28,24 +11,4 @@ func Solution(A []int, k int) []int {
 	}
 
 	return A
-}
-
-func makeArray() []int {
-	var A [1000]int
-	for i := 0; i < 1000; i++ {
-		A[i] = rand.Intn(2000) - 1000
-	}
-
-	return A[:100]
-}
-
-func main() {
-	var arr []int
-	var k int
-
-	for i := 1; i < 1000; i++ {
-		k = rand.Intn(1000)
-		arr = makeArray()
-		fmt.Println(measureTime(Solution, arr, k))
-	}
 }
