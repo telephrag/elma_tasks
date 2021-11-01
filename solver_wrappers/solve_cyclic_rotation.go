7457978fb70fb131cf873c867cc16dc3b5f686e5
trofchik <trofimov.nikita20@gmail.com>
2021-11-01 14:22:08 +0400

    Revert "Revert "Now program can correctly write solving results to slice""
    This reverts commit 5dd9e92a1c4cce74eefbaae2a6a6c9bf749cffe9.

--- solver_wrappers/solve_cyclic_rotation.go
+++ solver_wrappers/solve_cyclic_rotation.go
@@ -5,49 +5,63 @@ import (
 	"fmt"
 	"mservice/converters"
 	"mservice/solvers"
+	"reflect"
 	"sync"
 )
 
-func SolveForCyclicRotation(data [][]interface{}) error {
+func SolveForCyclicRotation(data [][]interface{}) ([][][]float64, []float64, error) {
 	var wg sync.WaitGroup
-	var res [][][]float64 = make([][][]float64, len(data))
 
-	for i := range res {
-		res[i] = make([][]float64, 2)
-		res[i][0] = make([]float64, len(data[i]))
-		res[i][1] = make([]float64, len(data[i]))
+	var resK []float64 = make([]float64, len(data))
+	var resA [][][]float64 = make([][][]float64, len(data))
+	for i := range resA {
+		resA[i] = make([][]float64, 2)
+		ra := reflect.ValueOf(data[i][0])
+		switch ra.Kind() {
+		case reflect.Slice:
+			resA[i][0] = make([]float64, ra.Len())
+			resA[i][1] = make([]float64, ra.Len())
+		default:
+			return nil, nil, errors.New("slice not found in the input data, slice was expected")
+		}
 	}
 
+	wg.Add(len(data))
 	for i := range data {
-		wg.Add(len(data))
-		go func(index int) error {
+		go func(index int) {
 			arr, err := converters.GetArrayAt(data, index)
 			if err != nil {
-				return errors.New("failed conversion to slice")
+				panic(err)
 			}
 
 			k, err := converters.GetFloatAt(data, index)
 			if err != nil {
-				return errors.New("failed conversion to slice")
+				panic(err)
 			}
 
-			fmt.Println(solvers.CyclicRotation(arr, k))
-			// TODO: Write to response here using lock
+			arrRot := make([]float64, len(arr))
+			copy(arrRot, arr)
+
+			solvers.CyclicRotation(arrRot, k)
+
+			copy(resA[index][0], arr)
+			copy(resA[index][1], arrRot)
+			resK[index] = k
 
 			wg.Done()
-
-			return nil
 		}(i)
 	}
+	wg.Wait()
 
-	return nil
-}
-
-func PrintResult(arr [][][]float64) {
-	for i := range arr {
-		fmt.Println("Index:  ", i)
-		fmt.Println("Data:   ", arr[i][0])
-		fmt.Println("Result: ", arr[i][1])
+	for i := range resA {
+		fmt.Println("Original:")
+		fmt.Println(resA[i][0])
+		fmt.Println("Rotation size:")
+		fmt.Println(resK[i])
+		fmt.Println("Rotated:")
+		fmt.Println(resA[i][1])
 		fmt.Println()
 	}
+
+	return resA, resK, nil
 }
