605cc8132d0d229e762eb5b893a65816dc7b0d68
trofchik <trofimov.nikita20@gmail.com>
2021-11-08 17:30:00 +0400

    Hid what wasn't ment to be seen.

--- config/config.go
+++ config/config.go
@@ -5,13 +5,9 @@ var ProtoHttp string = "http://"
 var LocalPublicAddr string = "localhost:3000"
 var LocalInternalAddr string = "localhost:8000"
 
-var DataProviderAddr string = "***REMOVED***"
-
 var CycliclShift string = "Циклическая ротация"
 var Warrentries string = "Чудные вхождения в массив"
 var SequenceCheck string = "Проверка последовательности"
 var AbscentElem string = "Поиск отсутствующего элемента"
 
 var TaskNames = [4]string{CycliclShift, Warrentries, SequenceCheck, AbscentElem}
-
-var UserName string = "telephrag"
